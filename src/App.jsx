902b279529c175307d8408437f38cfcd0a805bbe
Daryl Sabado <sabado114daryl@gmail.com>
2025-03-31 02:12:54 +0800

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ import Dashboard from "./pages/Dashboard";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
+      <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Registration />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Dashboard />} />
     </Routes>
   );
 }
